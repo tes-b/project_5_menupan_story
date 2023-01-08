dfdde254f517595045237a9f89ce7b2b7a0ed9eb
tes-b <tsyi0313@gmail.com>
2023-01-08 19:29:59 +0900

    update templates

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import requests
+import os
+import sys
+import urllib
 
 app = Flask(__name__)
 
@@ -12,30 +15,56 @@ def index():
 def dashboard():
     return render_template("dashboard.html"),200
 
-@app.route('/search/<rg>', methods=['GET'])
-def search(rg):
+
+@app.route('/search/<kw>', methods=['GET'])
+def search(kw):
     
 
     # 클라이언트 ID / Secret
-    client_id = "mxgcd10clr"
-    client_secret = "m3YwoBpcyRrId5zOXQP0DLSZzAGdmdpSzHrrKvky"
+    client_id = "rmn6_jUkJLItPG1eAU4z"
+    client_secret = "VsqpuaDsIl"
 
     # 좌표 (경도, 위도)
-    coords = "129.1133567,35.2982640"
-    output = "json"
-    orders = "=addr,admcode,roadaddr"
-    endpoint = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?"
-    url = f"{endpoint}coordsToaddr&coords={coords}&sourcecrs=epsg:4326&output={output}&orders={orders}"
+    encText = urllib.parse.quote("검색할 단어")
+    url = "https://openapi.naver.com/v1/search/blog?query=" + encText # JSON 결과
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
+
+    # output = "json"
+    # url = f"https://openapi.naver.com/v1/search/local.{output}?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=random"
+
 
     # 헤더
     headers = {
-        "X-NCP-APIGW-API-KEY-ID": client_id,
-        "X-NCP-APIGW-API-KEY": client_secret,
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
     }
 
     # 요청
     res = requests.get(url, headers=headers)
     res.json()
+
+
+    # 네이버 검색 API 예제 - 블로그 검색
+
+    client_id = "rmn6_jUkJLItPG1eAU4z"
+    client_secret = "VsqpuaDsIl"
+    encText = urllib.parse.quote(kw)
+    endpoint = "&display=10&start=1&sort=random"
+
+    url = "https://openapi.naver.com/v1/search/blog?query=" + encText + endpoint # JSON 결과
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
+
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
 
 if __name__ == '__main__':
     # app.run(debug=True)
